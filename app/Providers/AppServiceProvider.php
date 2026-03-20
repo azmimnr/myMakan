a4326738fd09741f2cb8451f58e8cd5ac13132bd
azmimnr <azmimnr@users.noreply.github.com>
2026-03-20 15:21:24 +0800

    fix: force https URL generation when APP_URL is https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $appUrl = (string) config('app.url', '');
+
+        // Behind reverse proxies, scheme detection can be inconsistent.
+        // If APP_URL is https, force URL generation to https to avoid mixed content.
+        if (str_starts_with($appUrl, 'https://')) {
+            URL::forceRootUrl($appUrl);
+            URL::forceScheme('https');
+        }
     }
 }
